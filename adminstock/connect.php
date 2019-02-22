f3758b48e24445683ba58a92e3359472aeff9838
Luigi <luigiazevedo97@gmail.com>
2019-02-22 08:56:02 -0300

    Add connect para teste

---
+++ adminstock/connect.php
@@ -0,0 +1,14 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "luigi");
+define("DB_PASSWORD", "calc33XGqqxFfvUj");
+define("DB_DATABASE", "montele_db");
+
+$mysqli = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
+if(!$mysqli){
+	echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	exit;
+}
+?>
