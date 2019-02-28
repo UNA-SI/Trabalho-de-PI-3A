8474ced46bbc94b43db00bac69fc4913501f11b7
Luigi <luigiazevedo97@gmail.com>
2019-02-28 10:33:45 -0300

    Tratamento e mudança no fix da tabela

--- adminstock/login/insert_cad.php
+++ adminstock/login/insert_cad.php
@@ -5,10 +5,6 @@ function gerarStringAleatoria($length = 10) {
     return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
 }
 
-
-
-
-
 // BUSCADO DADOS DO FORMULÁRIO DA PÁGINA DE CADASTRO E ARMAZENA EM NOVAS VARIAVÉIS
 $pri_nome = $_POST['pri_nome'];
 $ult_nome = $_POST['ult_nome'];
@@ -17,7 +13,7 @@ $login = $_POST['login'];
 $senha = hash('md5', $_POST['senha']);
 $pass_recovery = hash('md5', gerarStringAleatoria()); 
 // É REALIZADA UMA BUSCA NO BANCO SE A CONTA JÁ EXISTE
-$select = $mysqli->query("SELECT email FROM Usuario WHERE email='$email'");
+$select = $mysqli->query("SELECT email FROM usuario WHERE email='$email'");
 $result = $select->fetch_assoc();
 
 // CASO ELA EXISTA, O USUÁRIO É INFORMADO E REDIRECIONADO PARA A PÁGINA DE REGISTRO NOVAMENTE
@@ -29,7 +25,7 @@ if($result != "")
 		</script>";
 }
 // BUSCA NO BANCO SE UM LOGIN E SENHA IDENTICO EXISTE
-$select = $mysqli->query("SELECT login, senha FROM Usuario WHERE login = '$login' AND senha = '$senha'");
+$select = $mysqli->query("SELECT login, senha FROM usuario WHERE login = '$login' AND senha = '$senha'");
 $result = $select->fetch_assoc();
 
 // CASO ELA EXISTA, O USUÁRIO É INFORMADO E REDIRECIONADO PARA A PÁGINA DE REGISTRO NOVAMENTE
@@ -43,12 +39,19 @@ if($result != "")
 
 // CASO NÃO EXISTA, OS NOVOS DADOS SÃO INSERIDOS E O USUÁRIO RETORNA A TELA DE LOGIN
 else{ 
-	$insert = $mysqli->query("INSERT INTO Usuario (login, senha, pass_recovery, pri_nome, ult_nome, email) VALUES('$login', '$senha','$pass_recovery', '$pri_nome','$ult_nome', '$email')");
-	$mysqli->query($insert);	
-    echo "<script>
-	alert('Conta criada com sucesso!');
-	window.location.href='../../index.html';
-	</script>";
+	$insert = "INSERT INTO usuario (login, senha, recuperar_senha, pri_nome, ult_nome, email) VALUES('$login', '$senha','$pass_recovery', '$pri_nome','$ult_nome', '$email')";
+	if ($mysqli->query($insert) === TRUE) {
+		echo "<script>
+		alert('Conta criada com sucesso!');
+		window.location.href='../../index.html';
+		</script>";
+	} else {
+		echo "<script>
+		alert('Erro ao criar conta, tente novamente!');
+		window.location.href='register.html';
+		</script>";
+		//echo "Error: " . $insert . "<br>" . $mysqli->error; DEBUG
+	}
 }
 	
 $mysqli->close(); // fecha a conexao
