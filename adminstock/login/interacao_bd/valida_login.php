19b4697c43c9b1d49f2ce8fb3f32ed6192f13415
Luigi <luigiazevedo97@gmail.com>
2019-03-26 09:37:41 -0300

    Refatoração - valida_login.php

--- adminstock/login/interacao_bd/valida_login.php
+++ adminstock/login/interacao_bd/valida_login.php
@@ -1,30 +1,23 @@
 ﻿<?php
-require_once("../../requires/connect.php");
-require_once("../../requires/bcrypt.php");
-require_once("../../requires/functions.php");
-
+require_once("../../requires/connect.php");   // Conexao com o banco de dados
+require_once("../../requires/bcrypt.php");    // Classe Responsavel pela criptografia
+require_once("../../requires/functions.php"); // Funcoes
+// Busca de dados do usuario no bd
 $select = $mysqli->query("SELECT login, pri_nome, ult_nome, permissao, senha FROM usuario WHERE login ='{$_POST['login']}'");
 $result = $select->fetch_assoc();
-
+// Se a busca retornar vazia, e impressa uma mensagem de erro e redirecionamento
 if(empty($result)){	
 	alertaRedirect("Usu\u00e1rio incorreto ou n\u00e3o cadastrado!", "../../../index.html");	
 }
-
-// Recebe senha do formulário
-$senha = $_POST['senha'];
-// armazena a senha do banco na variavél hash
-$hash = $result['senha'];
-
-// verifica se as senhas são identicas
-if (Bcrypt::check($senha, $hash)){
-	// CASO O LOGIN ESTEJA CORRETO, OS DADOS SAO ARMAZENADOS EM UMA VARIAVEL DE SESSÃO
-	session_start();
-	$_SESSION['login'] = $_POST['login'];
-	$_SESSION['pri_nome'] = $result['pri_nome'];
+// Valida se a senha recebida esta correta
+if (Bcrypt::check($_POST['senha'], $result['senha'])){
+	session_start();                                 // Inicia sessao e armazena dados do usuario 
+	$_SESSION['login'] = $_POST['login'];            // em variaveis de ssesao, apos redireciona o usuario
+	$_SESSION['pri_nome'] = $result['pri_nome'];	 	
 	$_SESSION['ult_nome'] = $result['ult_nome'];
 	$_SESSION['permissao'] = $result['permissao'];
 	redirect("../../main/consulta_estoque.php");
-} else {
+} else { // caso a senha esteja incorreta, e impressa uma mensagem do erro e o usuario e redirecionado
 	alertaRedirect("Login ou senha incorretos. Tente Novamente.", "../../../index.html");
 }
 ?>
