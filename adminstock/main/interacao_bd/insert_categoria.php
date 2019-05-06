a5b356ff1619ec35868ccc435994ca3fdd7c5da0
Luigi <luigiazevedo97@gmail.com>
2019-05-06 10:01:03 -0300

    [Comment] Comentários adicionados ao código

--- adminstock/main/interacao_bd/insert_categoria.php
+++ adminstock/main/interacao_bd/insert_categoria.php
@@ -6,39 +6,43 @@
 	
 	Class CadastrarCategoria
 	{
-		private $mysqli;
-		private $descCat;
+		private $mysqli; // Conexao com o banco de dados
+		private $descCat; // Descricao da categoria
 		
-		public function __construct($mysqli, $postDescCat){
-			$this->mysqli = $mysqli;
+		public function __construct($mysqli, $postDescCat){ // Construtor
+			// Passa o valor do parametro para a variavel da classe
+			$this->mysqli = $mysqli; 
 			$this->descCat = $postDescCat;
 		}	
 		
-		function verificaCadastroDuplicado()
+		function verificaCadastroDuplicado() // Busca se existe categoria duplicada
 		{
-			// É REALIZADA UMA BUSCA NO BANCO SE A CATEGORIA JÁ EXISTE
+			// Uma busca e realizada para verificar se a dscricao da categoria ja foi utilizada
 			$select = $this->mysqli->query("
 				SELECT desc_categoria 
 				FROM categoria 
 				WHERE desc_categoria='{$this->descCat}'");
 			$result = $select->fetch_assoc();
-			if (!empty($result)){ // CASO ESTEJA VAZIO, IMPRIME MENSAGEM DE ERRO E REDIRECIONAMENTO
+			 // Caso a busca encontre um valor duplicado, o codigo para de executar e uma mensagem de erro e apresentada
+			if (!empty($result)){ 
 				Functions::alertaRedirect("Categoria j\u00e1 cadastrada!", "../cad_categoria.php");
 			}	
 		}
 	
-		function insereCategoria()
+		function insereCategoria() // Insere a categoria no banco de dados
 		{
+			// E realizada uma insercao no banco de dados da nova categoria selecionada
 			$insert = "INSERT INTO categoria (desc_categoria) 
 			VALUES('{$this->descCat}')";
+			 // Caso a insercao falhe, o codigo para de executar e uma mensagem de erro e apresentada
 			if($this->mysqli->query($insert) === FALSE){
 				Functions::alertaRedirect("Ocorreu um erro ao cadastrar a categoria, tente novamente!", "../cad_categoria.php");
 			}
+			// Caso a insercao seja bem sucedida, uma mensagem de sucesso e dada e apos o usuario e redirecionado
 			Functions::alertaRedirect("Categoria cadastrada com sucesso!", "../cad_categoria.php");
 		}
 	}
-
-$novaCategoria = new CadastrarCategoria($mysqli, $_POST['desc_cat']);	
-$novaCategoria->verificaCadastroDuplicado();
-$novaCategoria->insereCategoria(); 
+$novaCategoria = new CadastrarCategoria($mysqli, $_POST['desc_cat']); // Um novo objeto e criado e os parametro sao enviados ao construtor
+$novaCategoria->verificaCadastroDuplicado(); // Busca se a categoria ja esta cadastrada
+$novaCategoria->insereCategoria();  // Insere a categoria no banco de dados
 ?>
